c185d535e07b57b31ecb1617410dd7c8e9f2e129
Mayank SHarma <smayank252@gmail.com>
2023-05-03 20:43:52 +0530

    initial -4

--- mayank/settings.py
+++ mayank/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2iy2j5yk$)aen_honz^in#l%kv4$!el+11ptt--6xv3nf*sen6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['20.12.102.88', '127.0.0.0']
 
 
 # Application definition
